d2f76f1b466cb3ca916c6ce7bf767a2b27448317
Will <fowlerwill@gmail.com>
2024-01-07 20:51:33 -0700

    first rev

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
-# SvelteKit Demo app
+# Linking To Len
 
-The official demo app for SvelteKit, hosted on Vercel.
+## Summary
 
-## Deploy Your Own
+This is the repository for the website and materials associated with the childrens'
+book that we created to raise awareness for the Don Bosco Playground Fundraiser
 
-[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fvercel%2Fvercel%2Ftree%2Fmain%2Fexamples%2Fsveltekit-1&project-name=sveltekit-vercel&repository-name=sveltekit-vercel&demo-title=SvelteKit%20%2B%20Vercel&demo-url=https%3A%2F%2Fsveltekit-template.vercel.app%2F)
+https://www.justgiving.com/campaign/ccef-donboscoschool
 
-_Live Example: https://sveltekit-template.vercel.app_
+Please head there now to make a donation!
 
-## Developing
+## Developing the website
 
 Once you've created a project and installed dependencies with `npm install` (or `pnpm install` or `yarn`), start a development server:
 
@@ -19,7 +20,7 @@ npm run dev
 npm run dev -- --open
 ```
 
-## Building
+## Building the website
 
 To create a production version of your app:
 
@@ -28,9 +29,3 @@ npm run build
 ```
 
 You can preview the production build with `npm run preview`.
-
-## Speed Insights
-
-Once deployed on Vercel, you can benefit from [Speed Insights](https://vercel.com/docs/concepts/speed-insights) simply by navigating to Vercel's dashboard, clicking on the 'Speed Insights' tab, and enabling the product.
-
-You will get data once your application will be re-deployed and will receive visitors.
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,148 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+	import { Container, Image, Button, Input } from 'sveltestrap';
+	import cover from '$lib/images/cover.jpg';
+	
+	const epub = 'https://linking-to-len.s3.amazonaws.com/Linking_To_Len.epub';
+	const pdf = 'https://linking-to-len.s3.amazonaws.com/Linking_To_Len.pdf';
+	const fundraiser = 'https://www.justgiving.com/campaign/ccef-donboscoschool?utm_source=copyLink&utm_medium=one_page&utm_content=campaign/ccef-donboscoschool&utm_campaign=pfp-share&utm_term=ee727817bb294315b12147b6f77e2ff1';
+	const softcover = 'https://www.amazon.ca/dp/B0CR7W1BF3';
+
+	let stepOne = false;
+	let stepTwo = false;
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<title>Linking To Len</title>
+	<meta name="description" content="Linking To Len" />
 </svelte:head>
 
 <section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
+	<Container sm class="py-5 mx-auto">
+		<h1 class="display-5 fw-bold">
+			Linking to Len
+		</h1>
+		
+		<Image fluid src={cover} />
 
-		to your new<br />SvelteKit app
-	</h1>
+		<h2 class="my-4">
+			A project by the students, parents, and teachers of Don Bosco to raise
+			awareness for our playground fundraiser.
+		</h2>
 
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
+		<p class="my-4">
+			We are raising money to build a playground for the students of Don Bosco 
+			School in Calgary. To help raise awareness, we have created a children's 
+			book with the artwork from the students themselves a sprinkle of text from
+			ChatGPT, and some extensive editing from the parents.
+		</p>
 
-	<Counter />
+		<hr class="my-4" />
+
+		<h2 class="my-4">
+			How to help in a few simple steps:
+		</h2>
+
+		<ol class="my-4">
+			<li>Make a donation at the <a target="_blank" href={fundraiser}>fundraiser page</a>, then</li>
+			<li>Share this page with your friends and family, then</li>
+			<li>Download the eBook here, or order a printed copy</li>
+		</ol>
+
+		<hr class="my-4" />
+
+		<h2 class="my-4">
+			Ready?
+		</h2>
+
+		<h3 class="my-4">
+			Step 1: Make your donation
+		</h3>
+
+		<Button color="primary" size="lg" target="_blank" href={fundraiser} class="mx-auto">
+			Click here to donate
+		</Button>
+
+		<p class="my-4">
+			When you make your donation, you will be asked to leave a message, you can
+			write whatever you like, but we suggest you write something like this:
+
+			"Let's build a playground for the students of Don Bosco School! #LinkingToLen"
+		</p>
+
+		
+		<Input type="checkbox" label="I promise I have made a donation, and am ready to continue" bind:checked={stepOne} />
+
+		{#if stepOne}
+
+		<h3 class="my-4">
+			Step 2: Share this page
+		</h3>
+
+		<p class="my-4">
+			The best driver of donations is word of mouth. The more people who know
+			about this project, the more likely we are to reach our goal. So please 
+			share this page with your friends and family.
+		</p>
+
+		<p class="my-4">
+			You can share this page with your friends and family, and ask them to do the same.
+			using Facebook, Twitter, Instagram, TikTok, LinkedIn or any other social
+			media platform you like.
+		</p>
+
+
+		<Input type="checkbox" label="I promise I have shared this page, and am ready to continue" bind:checked={stepTwo} />
+
+		{/if}
+
+		{#if stepOne && stepTwo}
+		<h3 class="my-4">
+			Step 3: Get a copy of the book
+		</h3>
+
+		<p class="my-4">
+			We have created an eBook of the book, which you can download for free here.
+		</p>
+
+		<div class="mx-auto">
+
+			<Button color="primary" size="lg" target="_blank" href={epub}>
+				Click here to download the eBook (epub)
+			</Button>
+			
+			<Button color="primary" size="lg" target="_blank" href={pdf}>
+				Click here to download the eBook (pdf)
+			</Button>
+		</div>
+
+		<h4 class="my-4">
+			-- AND / OR --
+		</h4>
+
+		<p class="my-4">
+			We have also created a printed version of the book on Amazon that is being
+			sold for cost (we make no money from this, it is just to cover the cost of
+			printing and shipping).
+		</p>
+
+		<Button color="primary" size="lg" target="_blank" href={softcover}>
+			Softcover (Amazon)
+		</Button>
+
+		<Button disabled={true} color="secondary" size="lg">
+			Hardcover (Coming Soon)
+		</Button>
+
+		<p class="h1 my-5 text-center">
+			Thank you for your support!
+		</p>
+
+		{/if}
+
+	</Container>
+	
+
 </section>
 
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
 </style>
